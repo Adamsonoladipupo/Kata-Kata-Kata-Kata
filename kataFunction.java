081017c8a02b6a6c09fd096970fc762fcd74156a
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-31 07:01:17 +0100

    kataFunction.java

---
+++ kataFunction.java
@@ -0,0 +1,13 @@
+public class kataFunction{
+
+	public int AdditionCalculator(int a, int b){
+		return a + b;
+	}
+	public int DivisionCalculator(int a, int b){
+		return a / b;
+	}
+	public int SubtractionCalculator(int a, int b){
+		return a - b;
+	}
+
+}
