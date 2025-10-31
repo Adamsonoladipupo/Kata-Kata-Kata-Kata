b22b7dd9fb213e76c786f34972ad90dd4f31d5c2
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-31 06:59:47 +0100

    kata.java

---
+++ kata.java
@@ -0,0 +1,15 @@
+public class kata{
+	public static void main(String[] args){
+		double tempVar = 0;
+		double numberOfYears = 0;
+		int price = 50000;
+		int priceAndNumberOfItems = NumberOfItems * 50000;
+		while (price > 1){
+			tempVar = price * 0.08;
+			price -= tempVar;
+			numberOfYears ++;
+			tempVar = 0;
+		}
+		System.out.print(numberOfYears);
+	}
+}
