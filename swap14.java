25ba1eadb13da8eb087b03bd5fb40e40c52efeed
Ola-Plenty <abdulwahab.adamson180571004@st.lasu.edu.ng>
2025-11-03 05:50:24 +0100

    swap

---
+++ swap14.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class swap14{
+	public static void main(String[] args){
+		String[] words = {"banana", "apple", "cherry"};
+		Arrays.sort(words);
+		
+		System.out.print(Arrays.toString(words));
+		
+	}
+}
