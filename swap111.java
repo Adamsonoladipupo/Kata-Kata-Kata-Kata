859d491fbc87c4b6db547fcd3b2b6e0dd6a09c70
Ola-Plenty <abdulwahab.adamson180571004@st.lasu.edu.ng>
2025-11-03 05:50:04 +0100

    swap 111

---
+++ swap111.java
@@ -0,0 +1,13 @@
+public class swap111{
+	public static void main(String[] args){
+		System.out.print(swapNumbers(12,6));
+	}
+	public static int swapNumbers(int num1, int num2){
+		int tempVar = num1;
+		num1 = num2;
+		num2 = tempVar;
+
+		return num;
+		
+	}
+}
