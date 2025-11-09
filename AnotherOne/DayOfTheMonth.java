b21871b99baaf2511ba8d4f062d6d6c5b4b27877
Ola-Plenty <abdulwahab.adamson180571004@st.lasu.edu.ng>
2025-11-09 19:03:36 +0100

    day of the month

---
+++ AnotherOne/DayOfTheMonth.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DayOfTheMonth{
+	public static void main(String[] args){
+		Scanner inputCollector = new Scanner(System.in);
+
+		System.out.print("Enter todays day: ");
+		int todaysDate = inputCollector.nextInt();
+
+		System.out.print("Enter the number of days elapsed since today: ");
+		int futureData = inputCollector.nextInt();
+		
+		if (todaysDate == 1){
+			todaysDate = 
+		}
+
+/*
+		switch(todaysDate){
+			case 1->{}
+		}
+*/
+	}
+}
