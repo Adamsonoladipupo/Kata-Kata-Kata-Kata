d5ad375f60d3cd3f18376f89df71e97e8af0ee7a
OlaAdams <adamsonoladipupo>
2026-03-20 16:50:03 +0100

    Co-authored-by: Ola-Plenty <abdulwahab.adamson180571004@st.lasu.edu.ng>

--- models/Loop.java
+++ models/Loop.java
@@ -1,5 +1,6 @@
 public class Loop {
     public static void main(String[] args){
-        System.out.println("Hello World!")
+        System.out.println("Hello World!");
+        System.out.println("Let get the party started!");
     }
 }
